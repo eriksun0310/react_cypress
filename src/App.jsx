668a16ed7a004263cc5a61b80f8145efcb85f2d6
eriksun0310 <113155520+eriksun0310@users.noreply.github.com>
2024-06-12 14:51:46 +0800

    刪掉用不到的

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
@@ -9,8 +7,6 @@ function App() {
   // 顯示count
   const [showCount, setShowCount] = useState(false);
 
-  // 顯示列表
-  const [list, setList] = useState([])
   return (
     <>
       {showCount ? (
